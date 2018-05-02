56454c133b0f89d2635db8824868ad3da6503fde
Andrew Deloucas <andrew.deloucas@corp.salasobrien.com>
2018-05-01 20:47:37 -0700

    test1 correct

--- Tests/test1.py
+++ Tests/test1.py
@@ -12,16 +12,18 @@ import re
 class Test1(unittest.TestCase):  # pylint: disable=R0904
     """Class for unittest"""
 
-    def x(self):
+    def test1(self):
         ATF = [(r's,'), (r'S,'), (r't,'), (r'T,'), (r'sz'), (r'SZ'), (r's,a'), (r'as,-bat')]
         tittles =  [(r's,', 'ṣ'),  (r'S,', 'Ṣ'), (r't,', 'ṭ'), (r'T,', 'Ṭ'), (r'sz', 'š'), (r'SZ', 'Š')]
         target = [r'ṣ', r'Ṣ', r'ṭ', r'Ṭ', r'š', r'Š', r'ṣa', r'aṣ-bat']
+        
         self.tittles = \
             [(re.compile(regex), repl) for (regex, repl) in tittles]
+        
         for (pattern, repl) in self.tittles:
-            ATF = re.subn(pattern, repl, str(ATF))[0]
-        self.assertEqual(ATF, target)
+            ATF = re.subn(pattern, repl, str(ATF))[0]  
+        
+        self.assertEqual(ATF, str(target))
 
 if __name__ == '__main__':
     unittest.main()
-    
