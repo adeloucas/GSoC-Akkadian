8b3c339f01163afe5a03aa5f3d13adadf68f5a84
Andrew Deloucas <andrew.deloucas@corp.salasobrien.com>
2018-05-02 21:42:25 -0700

    test updating - test4 ready to move forward

--- Tests/test2.py
+++ Tests/test2.py
@@ -12,7 +12,10 @@ import re
 class Test2(unittest.TestCase):  # pylint: disable=R0904
     """Class for unittest"""
     def test2(self):
+
         ATF = ['szi3', 'lil2', 'bi2', 't,e4', 'u3', 'aga2', 'ARAD2', 'geme2', 'sig17', 'u3 _ku3-sig17', 'ra-pi2-qi2']
+        target = ['szì', 'líl', 'bí', 't,e₄', 'ù', 'ága', 'áRAD', 'géme', 'sig₁₇', 'ù _kù-sig₁₇', 'ra-pí-qí']
+        goal = str(target)
         
         accents =  [(r'[aA]([a-zA-Z][aeiouAEIOU][a-zA-Z][aeiouAEIOU])2', 'á\\1'), 
                     (r'[aA]([a-zA-Z][aeiouAEIOU][a-zA-Z][aeiouAEIOU])3', 'à\\1'),
@@ -57,16 +60,20 @@ class Test2(unittest.TestCase):  # pylint: disable=R0904
                     (r'[uU]([a-zA-Z][aeiouAEIOU])2', 'ú\\1'), 
                     (r'[uU]([a-zA-Z][aeiouAEIOU])3', 'ù\\1'),
                     (r'[uU]2', 'ú'), (r'[uU]3', 'ù'), (r'[uU]([a-zA-Z])2', 'ú\\1'), (r'[uU]([a-zA-Z])3', 'ù\\1'),
-                    (r'[uU]([sStT])([zZ,])2', 'ú\\1\\2'), (r'[uU]([sStT])([zZ,])3', 'ù\\1\\2')]
+                    (r'[uU]([sStT])([zZ,])2', 'ú\\1\\2'), (r'[uU]([sStT])([zZ,])3', 'ù\\1\\2'),
 
-        target = ['szì', 'líl', 'bí', 't,e4', 'ù', 'ága', 'áRAD', 'géme', 'sig17', 'ù _kù-sig17', 'ra-pí-qí']
+                    (r'([a-zA-Z])4', '\\1₄'), (r'([a-zA-Z])5', '\\1₅'), (r'([a-zA-Z])6', '\\1₆'), 
+                    (r'([a-zA-Z])7', '\\1₇'), (r'([a-zA-Z])8', '\\1₈'), (r'([a-zA-Z])9', '\\1₉'), 
+                    (r'([a-zA-Z])10', '\\1₁₀'), (r'([a-zA-Z])11', '\\1₁₁'), (r'([a-zA-Z])12', '\\1₁₂'), 
+                    (r'([a-zA-Z])13', '\\1₁₃'), (r'([a-zA-Z])14', '\\1₁₄'), (r'([a-zA-Z])15', '\\1₁₅'), 
+                    (r'([a-zA-Z])16', '\\1₁₆'),(r'([a-zA-Z])17', '\\1₁₇'), (r'([a-zA-Z])18', '\\1₁₈')]
 
         self.accents = \
             [(re.compile(regex), repl) for (regex, repl) in accents]
         for (pattern, repl) in self.accents:
             ATF = re.subn(pattern, repl, str(ATF))[0]
             
-        self.assertEqual(ATF, str(target))
+        self.assertEqual(ATF, goal)
 
 if __name__ == '__main__':
     unittest.main()
