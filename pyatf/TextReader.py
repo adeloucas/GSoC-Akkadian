105b055a328b1a1edc24aec172fbb71812fd3340
Andrew Deloucas <38022737+adeloucas@users.noreply.github.com>
2018-05-07 19:09:46 -0700

    Update TextReader.py

--- pyatf/TextReader.py
+++ pyatf/TextReader.py
@@ -1,6 +1,6 @@
 from pyatf.ATFConverter import ATFConverter
 
-file = r"C:\\Users\\andrew.deloucas\\GSoC-Akkadian\\texts\ARM1Akkadian.txt"
+file = ARM1Akkadian.txt
 ATF = open(file, 'r', encoding="utf8")
 original = ATF.read()
 normalize = ATFConverter()
