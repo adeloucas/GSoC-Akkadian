ec42b5bf23038809a95d1b4a48a07841e1488577
Andrew Deloucas <andrew.deloucas@corp.salasobrien.com>
2018-05-10 16:03:02 -0700

    sumerian adjustments

--- pyatf/TextReader.py
+++ pyatf/TextReader.py
@@ -1,8 +1,8 @@
 from pyatf.ATFConverter import ATFConverter
 
-file = r"C:\\Users\\andrew.deloucas\\GSoC-Akkadian\\texts\ARM1Akkadian.txt"
+file = r"C:\\Users\\andrew.deloucas\\GSoC-Akkadian\\texts\Akkadian.txt"
 ATF = open(file, 'r', encoding="utf8")
 original = ATF.read()
 normalize = ATFConverter()
 normalized = normalize.convert(original)
-print(normalized)
+print(normalized[0:20000])
--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ __license__ = 'MIT License. See LICENSE.'
 import unittest
 from pyatf.ATFConverter import ATFConverter
 
-class shts(unittest.TestCase):  # pylint: disable=R0904
+class test1(unittest.TestCase):  # pylint: disable=R0904
 
     def test1(self):
         ATF = ATFConverter()
@@ -15,18 +15,18 @@ class shts(unittest.TestCase):  # pylint: disable=R0904
 
         self.assertEqual(output, target)
 
-class signs(unittest.TestCase):  # pylint: disable=R0904
+class test2(unittest.TestCase):  # pylint: disable=R0904
 
     def test2(self):
         ATF = ATFConverter()
         text = ['szi3', 'lil2', 'bi2', 't,e4', 'u3', 'aga2', 'ARAD2', 'geme2', 'sig17', 'u3 _ku3-sig17', 'ra-pi2-qi2']
-        target = str(['šì', 'líl', 'bí', 'ṭe₄', 'ù', 'ága', 'áRAD', 'géme', 'sig₁₇', 'ù _kù-sig₁₇', 'ra-pí-qí'])
+        target = str(['šì', 'líl', 'bí', 'ṭe₄', 'ù', 'ága', 'áRAD', 'géme', 'sig₁₇', 'ù _KÙ-sig₁₇', 'ra-pí-qí'])
 
         output = ATF.convert(text)
 
         self.assertEqual(output, target)
 
-class determinatives(unittest.TestCase):  # pylint: disable=R0904
+class test3(unittest.TestCase):  # pylint: disable=R0904
 
     def test3(self):
         ATF = ATFConverter()
@@ -39,19 +39,21 @@ class determinatives(unittest.TestCase):  # pylint: disable=R0904
 
         self.assertEqual(output, target)
 
-class sumerian(unittest.TestCase):  # pylint: disable=R0904
+class test4(unittest.TestCase):  # pylint: disable=R0904
 
     def test4(self):
         ATF = ATFConverter()
         text = [r'_a-sza3 u8 udu hi-a_', r'_sze siki i3-gesz zu2-lum_', r'_ i-yu-ti-in _', r'_ sza szu-ba-at-_', 
                 r'_ szu-a-ti i-na _', r'_a-sza3 sze-gesz-i3_', r'_sipa u8 udu hi-a_', r'_a-sza3_ be-el _', r'_iti_ x x x _'
-                r'_ it-ti _',r'_ma-na ku3-babbar_']
+                r'_ it-ti _',r'_ma-na ku3-babbar_', r'_ga#-usz2_', r'_lú#_',
+                r'_{geš#}má#-hi-a gal_', r'_lú#-meš_']
         target = str([r'_A-ŠÀ U₈ UDU HI-A_', r'_ŠE SIKI Ì-GEŠ ZÚ-LUM_', r'_ i-yu-ti-in _', r'_ ša šu-ba-at-_', 
                       r'_ šu-a-ti i-na _', r'_A-ŠÀ ŠE-GEŠ-Ì_', r'_SIPA U₈ UDU HI-A_', r'_A-ŠÀ_ be-el _', r'_ITI_ x x x _'
-                      r'_ it-ti _', r'_MA-NA KÙ-BABBAR_'])
+                      r'_ it-ti _', r'_MA-NA KÙ-BABBAR_', r'_GA#-ÚŠ_', r'_LÚ#_',
+                    r'_ᵍᵉˢᶻ#MÁ#-HI-A GAL_', r'_LÚ#-MEŠ_'])
 
         output = ATF.convert(text)
-
+        self.maxDiff = None
         self.assertEqual(output, target)
 
 if __name__ == '__main__':
