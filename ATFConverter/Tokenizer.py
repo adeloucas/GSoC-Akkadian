80c79c732d4f7b86ece5843351bd74d691c20387
Andrew Deloucas <andrew.deloucas@corp.salasobrien.com>
2018-05-23 19:34:47 -0700

    touch up

--- ATFConverter/Tokenizer.py
+++ ATFConverter/Tokenizer.py
@@ -9,8 +9,6 @@ tokenizer = RegexpTokenizer(r'[[\[\]\<\>\(\)\?\#\!\s\-\{\}\|^\_]|\d*\.', gaps=Tr
 class Tokenizer(object):
     """Creates tokens from .txt"""
 
-    tokenizer = RegexpTokenizer(r'[[\[\]\<\>\(\)\?\#\!\s\-\{\}\|^\_]|\d*\.', gaps=True)
-
     def Hammurabi_tokenize_test(self, text):
         """This is obviously not a real function, but it works; don't know how to change line 16... """
         with open(r"C:\\Users\\andrew.deloucas\\GSoC-Akkadian\\texts\Akkadian.txt","r", encoding = "utf8") as File:
