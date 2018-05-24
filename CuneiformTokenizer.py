3271f6cce732b08b5e8d4edf5fd78aa0c50986f4
Andrew Deloucas <andrew.deloucas@corp.salasobrien.com>
2018-05-24 15:11:10 -0700

    adjustments

--- CuneiformTokenizer.py
+++ CuneiformTokenizer.py
@@ -2,11 +2,17 @@ from ATFConverter.ATFConverter import Tokenizer
 from ATFConverter.ATFConverter import ATFConverter
 
 sign_conversion = Tokenizer()
+convert = ATFConverter()
 
 with open(r"C:\\Users\\andrew.deloucas\\GSoC-Akkadian\\texts\\Akkadian.txt","r+", encoding = "utf8") as File:
 
     text2 = File.readlines()
-    text_selection2 = text2[32:42]
+    text_selection2 = text2[40:42]
     tokenized2 = sign_conversion.tokenizer(text_selection2)
+    conversion = convert.process(text_selection2)
 
+    print(text_selection2)
+    print()
     print(tokenized2)
+    print()
+    print(text_selection2)
